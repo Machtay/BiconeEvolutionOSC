026c976b795d1284d358c4f326b9def8bcbafc75
JulieRolla <rolla.3@osu.edu>
2019-12-26 03:08:55 -0500

    This commit has added Xmacros that I am unsure of the importance of. I've added them so that we have a commit in case we need it. If not, we can always go back to an earlier version in the git log

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/test.sh
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/test.sh
@@ -1,7 +1,7 @@
-RunName='Julie_12_2_2'           ## Replace when needed                                                                 
+RunName='Julie_12_2'           ## Replace when needed                                                                 
 TotalGens=6                     ## number of generations (after initial) to run through                                
 WorkingDir=`pwd` 
-NPOP=4                          ## number of individuals per generation; please keep this value below 99                
+NPOP=2                          ## number of individuals per generation; please keep this value below 99                
 FREQ=60                         ## frequencies being iterated over in XF (Currectly only affects the output.xmacro loop\
                                                                                                                        
 NNT=10000                          ##Number of Neutrinos Thrown in AraSim                                               
@@ -12,13 +12,19 @@ op/Run_Outputs/${RunName}
 
 
 
-cd "$WorkingDir"/Antenna_Performance_Metric
+#cd "$WorkingDir"/Antenna_Performance_Metric
        
         
-        for i in `seq 1 $NPOP`
-        do
-            InputFiles="${InputFiles}AraOut_${i}.txt "
-        done
+ #       for i in `seq 1 $NPOP`
+  #      do
+   #         InputFiles="${InputFiles}AraOut_${i}.txt "
+    #    done
 
 
-        ./fitnessFunction.exe $NPOP $ScaleFactor $WorkingDir/generationDNA.csv $InputFiles
+     #   ./fitnessFunction.exe $NPOP $ScaleFactor $WorkingDir/generationDNA.csv $InputFiles
+#cd "$WorkingDir"
+# rm runData.csv
+#python gensData.py 1
+cd Antenna_Performance_Metric
+#python LRPlot.py "$WorkingDir" "$WorkingDir"/Run_Outputs/$RunName 1 $NPOP
+python LRTPlot.py "$WorkingDir" "$WorkingDir"/Run_Outputs/$RunName 2 $NPOP  
